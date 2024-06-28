578aa949ce4028b726cca32d7d769e1c2fb1105b
thiago <thiago.bertachini@hotmail.com>
2024-06-28 20:04:40 -0300

    api for auth users

---
+++ login-page/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signUp",
+        component: SignUpComponent
+    },
+    {
+        path: "user",
+        component: UserComponent
+    }
+];
